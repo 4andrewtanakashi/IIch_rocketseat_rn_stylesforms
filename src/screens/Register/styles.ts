3b44e214b40f1653f4e078e2248764f277b8c85d
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2022-09-20 11:37:24 -0300

    feat(mod3): Componente de Botão:hammer:

--- src/screens/Register/styles.ts
+++ src/screens/Register/styles.ts
@@ -2,27 +2,30 @@ import styled from "styled-components/native";
 import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
-    flex: 1;
-    background-color: ${ ({theme}) => theme.colors.background };
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Header = styled.View`
-    background-color: ${ ({ theme }) => theme.colors.primary};
-    width: 100%;
-    height: ${RFValue(113)}px;
-    align-items: center;
-    justify-content: flex-end;
-    padding-bottom: 19px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  width: 100%;
+  height: ${RFValue(113)}px;
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: 19px;
 `;
 
 export const Title = styled.Text`
-    font-family: ${ ({ theme }) => theme.fonts.regular};
-    color: ${ ({ theme }) => theme.colors.shape};
-    font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
 `;
+
+export const Fields = styled.View``;
 
 export const Form = styled.View`
   flex: 1;
   width: 100%;
   padding: 24px;
+  justify-content: space-between;
 `;
