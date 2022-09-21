476dd592a832cf5002b0fce6e0b505b5dc3a4ba9
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2022-09-21 16:26:52 -0300

    feat(mod3): utilizando a tab navigation:hammer:

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from '@react-navigation/native';
 
 import {
   useFonts,
@@ -10,9 +11,7 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import {Dashboard} from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
-import { CategorySelect } from './src/screens/CategorySelect';
+import { AppRoutes } from './src/routes/app.routes'
 
 export default function App() {
   const [fontsLoader] = useFonts({
@@ -27,7 +26,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
