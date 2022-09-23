76913d8e3ba46a03ebb0d229c25a644f98ac7982
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2022-09-23 11:41:13 -0300

    feat(mod3): listando os totais por categoria:hammer:

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,20 +1,91 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import { Container, Header, Title } from './styles'
+import {
+  Container,
+  Header,
+  Title,
+  Content
+ } from './styles'
 import { HistoryCard } from '../../components/HistoryCard'
+import { TransactionCardProps } from '../../components/TransactionCard'
+import { categories } from '../../utils/categories'
+
+interface CategoryData {
+  key: string
+  name: string
+  total: string
+  color: string
+}
 
 export function Resume() {
+  const [time, setTime] = useState(2)
+  const [totalCategories, setTotalByCategories] = useState<CategoryData[]>([])
+
+  async function loadingData () {
+    const dataKey = '@gofinances:transactions'
+    const response = await AsyncStorage.getItem(dataKey)
+    const responseFormatted = response? JSON.parse(response) : []
+
+    const expensives = responseFormatted
+      .filter( (expensive : TransactionCardProps) => expensive.type === 'negative')
+    
+    const totalByCategory : CategoryData[] = []
+    
+    categories.forEach( category => {
+      let categorySum = 0
+      
+      expensives.forEach( (expensive : TransactionCardProps) => {
+        if (expensive.category === category.key) {
+          categorySum += Number(expensive.amount)
+        }
+      } )
+
+      if (categorySum > 0) {
+        const total = categorySum
+          .toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          } )
+
+        totalByCategory.push( {
+          key: category.key,
+          name: category.name,
+          color: category.color,
+          total
+        } )
+      }
+    } )
+    setTotalByCategories(totalByCategory)
+  }
+
+  useEffect( () => {
+    const t = setInterval(
+      () => setTime(2),
+      200,
+    );
+    loadingData()
+    return () => clearInterval(t)
+  }, [] )
+
   return (
     <Container>
       <Header>
         <Title>Resumo por categoria</Title>
       </Header>
 
-      <HistoryCard 
-        title={'Compras'}
-        amount={'R$ 150,50'}
-        color={'red'}
-      />      
+      <Content>
+        {
+          totalCategories.map( item =>
+            <HistoryCard
+              key={item.key}
+              title={item.name}
+              amount={item.total}
+              color={item.color}
+            />
+          )
+        }
+      </Content>
     </Container>
   )
 }
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -4,7 +4,7 @@ import { RFValue } from 'react-native-responsive-fontsize';
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background};
-`;
+`
 
 export const Header = styled.View`
   background-color: ${({ theme }) => theme.colors.primary};
@@ -13,10 +13,14 @@ export const Header = styled.View`
   align-items: center;
   justify-content: flex-end;
   padding-bottom: 19px;
-`;
+`
 
 export const Title = styled.Text`
   font-family: ${({ theme }) => theme.fonts.regular};
   color: ${({ theme }) => theme.colors.shape};
   font-size: ${RFValue(18)}px;
-`;
+`
+
+export const Content = styled.ScrollView.attrs( {
+  contentContainerStyle: {padding: 24, flex: 1}
+} )``
