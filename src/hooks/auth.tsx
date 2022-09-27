7f908460346757ab7eee6f7f560b53567b053e03
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2022-09-27 14:41:41 -0300

    feat(mod4): conectando com uma conta apple:hammer:

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -6,6 +6,9 @@ import React,
     useState
   } from 'react'
 import * as AuthSession from 'expo-auth-session'
+import * as AppleAuthentication from 'expo-apple-authentication'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 
 const { CLIENT_ID } = process.env
 const { REDIRECT_URI } = process.env
@@ -24,6 +27,7 @@ interface User {
 interface IAuthContextData {
   user: User
   signInWithGoogle(): Promise<void>
+  signInWithApple(): Promise<void>
 }
 
 interface AuthrizationResponse {
@@ -48,17 +52,42 @@ function AuthProvider ( { children } : AuthProviderProps ) {
       if (type === 'success') {
         const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`)
         const userInfo = await response.json()
-        
-        setUser( {
+        const userLogged : User = {
           id: userInfo.id,
           email: userInfo.email,
           name: userInfo.given_name,
           photo: userInfo.picture
-        } )
+        }
+        setUser(userLogged)
+        await AsyncStorage.setItem('@gofinances:user', JSON.stringify(userLogged))
       }
-
     } catch (err : unknown) {
       throw new Error(err as string)
+    }
+  }
+
+  async function signInWithApple () {
+    try {
+      const credential = await AppleAuthentication.signInAsync( {
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+          AppleAuthentication.AppleAuthenticationScope.EMAIL
+        ]
+      } )
+
+      if ( credential ) {
+        const userLogged = {
+          id: String(credential.user),
+          email: credential.email!,
+          name: credential.fullName!.givenName!,
+          photo: undefined
+        }
+        setUser(userLogged)
+        await AsyncStorage.setItem('@gofinances:user', JSON.stringify(userLogged))
+      }
+      
+    } catch ( error ) {
+      throw new Error(error as string)
     }
   }
 
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -20,12 +20,22 @@ import { SignInSocialButtom } from '../../components/SignInSocialButtom'
 import { useAuth } from '../../hooks/auth'
 
 export function SignIn() {
-  const { signInWithGoogle } = useAuth()
+  const { signInWithGoogle, signInWithApple } = useAuth()
 
   async function handleSignInWithGoogle () {
 
     try {
       await signInWithGoogle()
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Não foi possível conectar a conta google')
+    }
+  }
+
+  async function handleSignInWithApple () {
+
+    try {
+      await signInWithApple()
     } catch (error) {
       console.log(error)
       Alert.alert('Não foi possível conectar a conta google')
@@ -61,7 +71,11 @@ export function SignIn() {
             svg={GoogleSvg}
             onPress={handleSignInWithGoogle}
           />
-          <SignInSocialButtom title={'Entrar com Apple'} svg={AppleSvg} />
+          <SignInSocialButtom
+            title={'Entrar com Apple'}
+            svg={AppleSvg}
+            onPress={handleSignInWithApple}
+          />
         </FooterWrapper>
       </Footer>
     </Container>
