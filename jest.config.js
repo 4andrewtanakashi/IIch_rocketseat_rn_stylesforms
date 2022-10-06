4a3f6cba3361d7d691f8d42cc0ce9ef987fc717f
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2022-10-05 22:33:22 -0300

    feat(ch5mod1):  cobertura de codigo :pencil:

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports =  {
   preset: "jest-expo",
+
   testPathIgnore: [
     '/node_modules',
     '/android',
@@ -8,5 +9,15 @@ module.exports =  {
   setupFilesAfterEnv: [
       "@testing-library/jest-native/extend-expect",
       "jest-styled-components"
+  ],
+
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "src/**/*.spec.tsx",
+    "src/**/*.test.tsx"
+  ],
+  coverageReporters: [
+    "lcov"
   ]
 }
